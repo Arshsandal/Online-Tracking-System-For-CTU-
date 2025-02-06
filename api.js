66cfb50331f8b2be2ec41ff1f7880af5eec514ae
Arshsandal <arshsandal50@gmail.com>
2025-02-06 15:40:30 +0530

    Fix Filter in index.html file, Integrate API's for various Stations and improve the logic of filter in main.js file

---
+++ api.js
@@ -0,0 +1,45 @@
+
+const selectFromStation = document.querySelector('#stationFrom')
+var placeholderOption = document.createElement("option");
+placeholderOption.value = "";
+placeholderOption.textContent = "Select Station From";
+placeholderOption.disabled = true;
+placeholderOption.selected = true;
+selectFromStation.appendChild(placeholderOption);
+fetch('../api/formatted_bus_routes.json')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach((item) => {
+      const Rno = item.route_number;
+      const From = item.start;
+      const option = document.createElement("option")
+      option.value = From;
+      option.textContent = `${Rno} ${From}`;
+      selectFromStation.appendChild(option)
+    });
+  })
+
+
+
+
+
+  
+
+const selectToStation = document.querySelector('#selectToStation')
+var placeholderOption = document.createElement("option");
+placeholderOption.value = "";
+placeholderOption.textContent = "Select Station To";
+placeholderOption.disabled = true;
+placeholderOption.selected = true;
+selectToStation.appendChild(placeholderOption);
+fetch('../api/formatted_bus_routes.json')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach((item) => {
+      const To = item.start;
+      const option = document.createElement("option")
+      option.value = To;
+      option.textContent = To;
+      selectToStation.appendChild(option)
+    });
+  })
